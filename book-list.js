825cb6a4d7bcbbe528414d8b31e2b1a3397f2a2d
kimaiga <nelsonkimaiga@aol.com>
2016-01-16 16:16:12 +0300

    Tested static data

--- book-list.js
+++ book-list.js
@@ -14,6 +14,24 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.hello.rendered = function() {
+    Session.setDefault('books', [
+      {title: "To Kill a Mockingbird", author: "Harper Lee"},
+      {title: "1984", author: "George Orwell"},
+      {title: "The Lord of the Rings", author: "J. R. R. Tolkien"},
+      {title: "The Catcher in the Rye", author: "J. D. Salinger"},
+      {title: "The Great Gatsby", author: "F. Scott Fitzgerald"}
+    ]);
+  }; 
+
+  Template.hello.helpers({
+    books: function () {
+      return Session.get('books');
+    }
+  });
+
+
 }
 
 if (Meteor.isServer) {
